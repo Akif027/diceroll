530c6ab31ad646bf83a498437ebfd2e6957fc4c9
akif shaikh <66055292+ZACK8149@users.noreply.github.com>
2021-05-18 14:32:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+$(".start").click(function(){
+    var randomnumber1=Math.floor(Math.random() *6)+1;
+
+    var randomImagesource="images/dice"+ randomnumber1 + ".png";
+    
+    var image1 = document.querySelectorAll("img")[0];
+    image1.setAttribute("src",randomImagesource);
+    
+    var randomnumber2=Math.floor(Math.random() *6)+1;
+    var randomImagesource2="images/dice"+ randomnumber2 + ".png";
+    
+    document.querySelectorAll("img")[1].setAttribute("src",randomImagesource2);
+    
+    
+    
+    if (randomnumber1>randomnumber2){
+        document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+    }else if(randomnumber1 < randomnumber2){
+        document.querySelector("h1").innerHTML="🚩Player 2 Wins!";
+    }else{
+        document.querySelector("h1").innerHTML="Draw";
+    }
+    
+})
+
